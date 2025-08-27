01049ba734f8887f63306ca09047d432bed0198b
Davie <daktaridavie@gmail.com>
2025-08-27 21:51:04 +0300

    finalized the game logic

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
-let humanScore = 0;
-let computerScore = 0;
 
-function playRound(humanChoice, computerChoice){
-   if(humanChoice === computerChoice){
-    console.log("This round was a tie");
-    return "Tie";
-   }
-   else if(humanChoice === "rock" && computerChoice === "paper"
-    || humanChoice === "paper" && computerChoice === "scissors"
-    || humanChoice === "scissors" && computerChoice === "rock"
-   ){
-    console.log("Computer has won");
-    return "You lose!";
-   }
-     else if(humanChoice === "paper" && computerChoice === "rock"
-    || humanChoice === "scissors" && computerChoice === "paper"
-    || humanChoice === "rock" && computerChoice === "scissors"
-   ){
-     console.log("Computer has lost, you won");
-     return "You win!"
-   }
-    
-};
+
+function playGame(){
+   let humanScore = 0;
+   let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice){
+      humanChoice = humanChoice.toLowerCase();
+      if(humanChoice === computerChoice){
+        console.log("This round was a tie");
+        return "Tie";
+      }
+      else if(humanChoice === "rock" && computerChoice === "paper"
+        || humanChoice === "paper" && computerChoice === "scissors"
+        || humanChoice === "scissors" && computerChoice === "rock"
+      ){
+        console.log("Computer has won");
+        computerScore += 1;
+        return "You lose!";
+      }
+        else if(humanChoice === "paper" && computerChoice === "rock"
+        || humanChoice === "scissors" && computerChoice === "paper"
+        || humanChoice === "rock" && computerChoice === "scissors"
+      ){
+        console.log("Computer has lost, you won");
+        humanScore += 1;
+        return "You win!"
+      }else {
+        return "invalid input"
+      } 
+    };
+    for(let i = 0;i < 5; i++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection)
+    }
+    console.log(`You scored ${humanScore}`)
+    console.log(`The computer scored ${computerScore}`)
+}
+playGame()
+
+
+
 
 function getComputerChoice(){
   const randomNum = Math.floor(Math.random() * 9);
@@ -37,7 +56,7 @@ function getComputerChoice(){
 };getComputerChoice()
 
 function getHumanChoice(){
-  return prompt("Rock-paper-scissors?").toLowerCase();     
+  return prompt("Rock-paper-scissors?");     
 }getHumanChoice()
 
 
